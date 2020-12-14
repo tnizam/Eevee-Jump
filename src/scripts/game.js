47779567d63193554f0429655cf4792449b25105
tnizam <tahmina.nizam94@gmail.com>
2020-12-14 04:50:41 -0500

    edits

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,11 +2,23 @@
 const canvas = document.querySelector("canvas")
 const context = canvas.getContext("2d");
  
+const playerSprite = new Image();
+playerSprite.src = "src/images/eevee2.png";
+const background = new Image();
+background.src = "src/images/bg-img.jpg";
+    
+const platformSprite1 = new Image();
+platformSprite1.src = "src/images/13.png";
+const platformSprite2 = new Image();
+platformSprite2.src = "src/images/14.png";
+const platformSprite3 = new Image();
+platformSprite3.src = "src/images/15.png";
+const fireStone = new Image();
+fireStone.src = "src/images/stones1.png";
 
 canvas.height = 400;
 canvas.width = 600;
-// context.fillStyle = "#202020";
-// context.fillRect(0,0,600,400);
+
 const keys = [];
 
 const player = {
@@ -22,21 +34,8 @@ const player = {
     jump: true
 }
 
-const playerSprite = new Image();
-playerSprite.src = "src/images/eevee2.png";
-const background = new Image();
-background.src = "src/images/bg-img.jpg";
 
-const platformSprite1 = new Image();
-platformSprite1.src = "src/images/13.png";
-const platformSprite2 = new Image();
-platformSprite2.src = "src/images/14.png";
-const platformSprite3 = new Image();
-platformSprite3.src = "src/images/15.png";
-const fireStone = new Image();
-fireStone.src = "src/images/stones1.png";
-
-function drawSprite(img, spriteX, spriteY, spriteW, spriteH, dX, dY, dW, dH) {
+function drawChar(img, spriteX, spriteY, spriteW, spriteH, dX, dY, dW, dH) {
     context.drawImage(img, spriteX, spriteY, spriteW, spriteH, dX, dY, dW, dH);
 }
 
@@ -58,35 +57,32 @@ window.addEventListener("keyup", function(event){
 
 function movePlayer() {
     if(keys["ArrowLeft"] && player.x > 0) {
-        // player.x -= player.x_velocity;
         player.x_velocity -= 0.5;
-        // player.frameY = 1;
         player.frameY = 0;
         player.moving = true;
         player.jumping = true;
 
     }
     if(keys["ArrowRight"]) { // && player.x < canvas.width - player.width
-        // player.x += player.x_velocity;
         player.x_velocity += 0.5;
         player.frameY = 1;
-        // player.frameX = 1;
         player.moving = true;
     }
     if(keys["ArrowUp"] && player.jumping == false) {
         player.y_velocity -= 30;
-        // player.frameY = 2;
         player.jumping = true;
     }
-    player.y_velocity += 1.5; // gravity
+
+    // gravity
+    player.y_velocity += 1.5; 
     player.x += player.x_velocity;
     player.y += player.y_velocity;
     player.x_velocity *= 0.9;
     player.y_velocity *= 0.9;
     
-    if (player.y > 400 - 65 - 67) { // 32 is top cord of rect
+    if (player.y > 268) { 
         player.jumping = false;
-        player.y = 400 - 65 - 67;
+        player.y = 268;
         player.y_velocity = 0;
     }
 }
@@ -200,7 +196,7 @@ function animate() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(background, 0, 0, canvas.width, canvas.height);
     context.beginPath();
-    drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+    drawChar(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
     movePlayer();
     PlayerFrame();
 
