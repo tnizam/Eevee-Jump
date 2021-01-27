b3dad431f200d11392c90dede113ab2f49e4e771
tnizam <tahmina.nizam94@gmail.com>
2021-01-27 14:53:15 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "./styles/index.scss";
 // import "../src/scripts/player"
 
-import "./styles/style.css";
+// import "./styles/style.css";
+// import "../src/styles/index.scss"
 
-import '../src/scripts/game';
+// import '../src/scripts/game';
 // import '../src/scripts/main_player';
 
